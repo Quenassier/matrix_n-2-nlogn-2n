92c165dabd94a7a92c7402dbc776bf3c5449c0c0
Quenassier <112952435+Quenassier@users.noreply.github.com>
2023-10-02 23:27:04 +0700

    Update nlogn.cpp

--- nlogn.cpp
+++ nlogn.cpp
@@ -13,18 +13,18 @@ bool search(int row[], int target)
 
     while (left <= right) 
     {
-        int mid = left + (right - left) / 2;
-        if (row[mid] == target) {
-            return true;
-        }
-        else if (row[mid] < target) 
-        {
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
-        }
-        }
+    int mid = left + (right - left) / 2;
+    if (row[mid] == target) {
+    return true;
+    }
+    else if (row[mid] < target) 
+    {
+    left = mid + 1;
+    }
+    else {
+    right = mid - 1;
+    }
+    }
 
     return false;
 }
@@ -33,11 +33,13 @@ bool search(int row[], int target)
 bool number(int matrix[n][n], int target) 
 {
 
-    for (int i = 0; i < n; ++i) {
-        if (search(matrix[i], target)) {
-            return true;
-        }
-        }
+    for (int i = 0; i < n; ++i) 
+    { 
+    if (search(matrix[i], target)) 
+    {
+    return true;
+    }
+    }
 
     return false;
 }
@@ -45,19 +47,19 @@ bool number(int matrix[n][n], int target)
 int main() {
     int arr[n][n] = 
   {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
     };
 
     int target;
     cout << "Введите искомое число";
     cin >> target;
     if (number(arr, target)) {
-        cout << "Число " << target << " найдено в матрице." << endl;
+    cout << "Число " << target << " найдено в матрице." << endl;
     }
     else {
-        cout << "Число " << target << " не найдено в матрице." << endl;
+    cout << "Число " << target << " не найдено в матрице." << endl;
     }
 
     return 0;
