9a6a8f9d4b64da1eed56e49bc8090e5fffbcecaa
Quenassier <112952435+Quenassier@users.noreply.github.com>
2023-10-02 23:01:23 +0700

    Create 2n.cpp

---
+++ 2n.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+
+const int n = 3;
+
+
+bool MatrixNumberIs(int arr[n][n], int target) {
+
+    for (int i = 0; i < n; ++i) {
+
+    for (int j = 0; j < n; ++j) {
+
+    if (array[i][j] == target) {
+
+    return true;
+    }
+    }
+    }
+
+    for (int j = 0; j < n; ++j) {
+
+    for (int i = 0; i < n; ++i) {
+
+    if (array[i][j] == target) {
+
+    return true;
+    }
+    }
+    }
+
+    
+    return false;
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    int array[n][n] = {
+           {1, 2, 3},
+           {4, 5, 6},
+           {7, 8, 9}
+    };
+
+    int target;
+    cout << "Введите искомое число";
+    cin >> target;
+
+    if (MatrixNumberIs, target)) {
+        cout << " Число" << target << " найдено в матрице. " << endl;
+    }
+    else {
+        cout << " Число " << target << " не найдено в матрице. " << endl;
+    }
+
+    return 0;
+}
